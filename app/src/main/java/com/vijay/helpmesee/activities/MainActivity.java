fa210264781253bb33cb5070f0e9e13614be3f45
Nagra, Vijay <Vijay.X.Nagra.-ND@disney.com>
2017-11-03 18:07:45 -0700

    git init

---
+++ app/src/main/java/com/vijay/helpmesee/activities/MainActivity.java
@@ -0,0 +1,17 @@
+
+package com.vijay.helpmesee.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vijay.helpmesee.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
