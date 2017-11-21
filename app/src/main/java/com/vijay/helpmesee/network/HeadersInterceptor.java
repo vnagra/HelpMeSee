3ffc7e0b6e35d24b86ab2a4182fea260d3f59861
Nagra, Vijay <Vijay.X.Nagra.-ND@disney.com>
2017-11-20 23:32:57 -0800

    Adding HeadersInterceptor for okhttp3

---
+++ app/src/main/java/com/vijay/helpmesee/network/HeadersInterceptor.java
@@ -0,0 +1,17 @@
+package com.vijay.helpmesee.network;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+/**
+ * Created by nagrv001 on 11/20/17.
+ */
+
+public class HeadersInterceptor implements Interceptor {
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return null;
+    }
+}
